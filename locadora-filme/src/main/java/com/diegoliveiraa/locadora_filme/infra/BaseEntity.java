57dc013f5d83b1945469169001fe4c5f8e01f9ef
Diego Oliveira <diegoliveiraa1@gmail.com>
2024-12-17 11:12:54 -0300

    feat: annotation adjust

--- locadora-filme/src/main/java/com/diegoliveiraa/locadora_filme/infra/BaseEntity.java
+++ locadora-filme/src/main/java/com/diegoliveiraa/locadora_filme/infra/BaseEntity.java
@@ -3,14 +3,14 @@ package com.diegoliveiraa.locadora_filme.infra;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
-@Getter
-@Setter
 public abstract class BaseEntity {
 
     private LocalDateTime timestamp;
