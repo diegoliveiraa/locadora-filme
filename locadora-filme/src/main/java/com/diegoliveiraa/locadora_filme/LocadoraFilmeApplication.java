490f739b71d10bd3b5fcefdaf55bbc6d202bb4ac
Diego Oliveira <diegoliveiraa1@gmail.com>
2024-12-16 21:49:40 -0300

    feat: initial configuration

---
+++ locadora-filme/src/main/java/com/diegoliveiraa/locadora_filme/LocadoraFilmeApplication.java
@@ -0,0 +1,13 @@
+package com.diegoliveiraa.locadora_filme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoraFilmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoraFilmeApplication.class, args);
+	}
+
+}
---
+++ locadora-filme/src/test/java/com/diegoliveiraa/locadora_filme/LocadoraFilmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegoliveiraa.locadora_filme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraFilmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
