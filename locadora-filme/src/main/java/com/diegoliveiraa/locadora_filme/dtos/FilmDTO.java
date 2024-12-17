cc036104975ff916e237ce61a6a10946bbfdd2c5
Diego Oliveira <diegoliveiraa1@gmail.com>
2024-12-17 16:52:41 -0300

    feat: create attributes

--- locadora-filme/src/main/java/com/diegoliveiraa/locadora_filme/dtos/FilmDTO.java
+++ locadora-filme/src/main/java/com/diegoliveiraa/locadora_filme/dtos/FilmDTO.java
@@ -1,4 +1,10 @@
 package com.diegoliveiraa.locadora_filme.dtos;
 
-public record FilmDTO() {
+import com.diegoliveiraa.locadora_filme.infra.FilmType;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record FilmDTO(String id, String title, String gender, LocalDate releaseDate
+        , FilmType filmType, Long quantity, BigDecimal locationPrice) {
 }
