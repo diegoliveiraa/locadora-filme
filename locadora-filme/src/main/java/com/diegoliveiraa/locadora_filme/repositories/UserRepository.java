63df59923bb1c4113264b90e005fceaaa9f4d6da
Diego Oliveira <diegoliveiraa1@gmail.com>
2024-12-17 13:05:39 -0300

    feat: add query annotation

--- locadora-filme/src/main/java/com/diegoliveiraa/locadora_filme/repositories/UserRepository.java
+++ locadora-filme/src/main/java/com/diegoliveiraa/locadora_filme/repositories/UserRepository.java
@@ -3,5 +3,9 @@ package com.diegoliveiraa.locadora_filme.repositories;
 import com.diegoliveiraa.locadora_filme.entitys.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByDocument(String document);
 }
