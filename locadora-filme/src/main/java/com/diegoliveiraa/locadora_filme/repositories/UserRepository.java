4b54751c9eee41328c1e474df9b25df697327209
Diego Oliveira <diegoliveiraa1@gmail.com>
2024-12-17 16:56:53 -0300

    feat: add query syntax

--- locadora-filme/src/main/java/com/diegoliveiraa/locadora_filme/repositories/UserRepository.java
+++ locadora-filme/src/main/java/com/diegoliveiraa/locadora_filme/repositories/UserRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, String> {
 
     Optional<User> findByDocument(String document);
+
+    Optional<User> findById(String id);
 }
