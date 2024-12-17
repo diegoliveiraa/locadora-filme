7f109bacb926fae810bfe16629cdbb2711c40829
Diego Oliveira <diegoliveiraa1@gmail.com>
2024-12-17 13:05:18 -0300

    feat: add parameters

--- locadora-filme/src/main/java/com/diegoliveiraa/locadora_filme/dtos/UserDTO.java
+++ locadora-filme/src/main/java/com/diegoliveiraa/locadora_filme/dtos/UserDTO.java
@@ -1,4 +1,10 @@
 package com.diegoliveiraa.locadora_filme.dtos;
 
-public record UserDTO() {
+import com.diegoliveiraa.locadora_filme.infra.UserType;
+
+import java.util.Date;
+
+public record UserDTO(String username, String password, String firstname, String lastname, String document,
+                      String phone, UserType userType, String cep, String address, String number,
+                      String complement, String city, Date birthday) {
 }
