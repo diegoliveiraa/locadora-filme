29bd7cf2bfde65332cd20fe161780ce7ccab48ff
Diego Oliveira <diegoliveiraa1@gmail.com>
2024-12-17 16:53:50 -0300

    feat: add DTOs attributes

--- locadora-filme/src/main/java/com/diegoliveiraa/locadora_filme/dtos/LocationDTO.java
+++ locadora-filme/src/main/java/com/diegoliveiraa/locadora_filme/dtos/LocationDTO.java
@@ -1,4 +1,16 @@
 package com.diegoliveiraa.locadora_filme.dtos;
 
-public record LocationDTO() {
+import com.diegoliveiraa.locadora_filme.infra.DevolutionStatus;
+import com.diegoliveiraa.locadora_filme.infra.PaymentStatus;
+import com.diegoliveiraa.locadora_filme.infra.PaymentType;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+public record LocationDTO(String id, String user, String renter
+        , List<LocationFilmDTO> film
+        , PaymentType paymentType, PaymentStatus paymentStatus, DevolutionStatus devolutionStatus
+        , BigDecimal totalPayment, Date dateDevolution) {
 }
+
