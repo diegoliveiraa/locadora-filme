14ea440415611644fc4d398f03d2e22370933689
Diego Oliveira <diegoliveiraa1@gmail.com>
2024-12-17 16:52:57 -0300

    feat: add syntax query

--- locadora-filme/src/main/java/com/diegoliveiraa/locadora_filme/repositories/FilmRepository.java
+++ locadora-filme/src/main/java/com/diegoliveiraa/locadora_filme/repositories/FilmRepository.java
@@ -3,5 +3,10 @@ package com.diegoliveiraa.locadora_filme.repositories;
 import com.diegoliveiraa.locadora_filme.entitys.Film;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FilmRepository extends JpaRepository<Film, String> {
+    Optional<Film> findByTitle(String title);
+
+    Optional<Film> findById(String id);
 }
