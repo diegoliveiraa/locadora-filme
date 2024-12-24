2faac7674ba13603b4f1b3bb5aa4938c254fff18
Diego Oliveira <diegoliveiraa1@gmail.com>
2024-12-24 13:58:16 -0300

    feat: create a personalize query

--- locadora-filme/src/main/java/com/diegoliveiraa/locadora_filme/repositories/LocationRepository.java
+++ locadora-filme/src/main/java/com/diegoliveiraa/locadora_filme/repositories/LocationRepository.java
@@ -2,6 +2,16 @@ package com.diegoliveiraa.locadora_filme.repositories;
 
 import com.diegoliveiraa.locadora_filme.entitys.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 public interface LocationRepository extends JpaRepository<Location, String> {
+    Optional<Location> findLocationByRenter(String renter);
+
+    @Query("SELECT l FROM Location l WHERE l.devolutionStatus = :devolutionStatus AND l.dateDevolution < :currentDate")
+    List<Location> findOverdueLocations(@Param("devolutionStatus") String devolutionStatus, @Param("currentDate") LocalDateTime currentDate);
 }
