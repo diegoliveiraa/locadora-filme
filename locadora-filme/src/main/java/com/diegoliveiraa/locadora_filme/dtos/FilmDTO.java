917bc8b81e89f3780c2ddc1cc23ac96ea069a089
Diego Oliveira <diegoliveiraa1@gmail.com>
2024-12-24 13:55:53 -0300

    feat: remove quantity and add film status

--- locadora-filme/src/main/java/com/diegoliveiraa/locadora_filme/dtos/FilmDTO.java
+++ locadora-filme/src/main/java/com/diegoliveiraa/locadora_filme/dtos/FilmDTO.java
@@ -1,10 +1,11 @@
 package com.diegoliveiraa.locadora_filme.dtos;
 
+import com.diegoliveiraa.locadora_filme.infra.FilmStatus;
 import com.diegoliveiraa.locadora_filme.infra.FilmType;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public record FilmDTO(String id, String title, String gender, LocalDate releaseDate
-        , FilmType filmType, Long quantity, BigDecimal locationPrice) {
+public record FilmDTO(String id, String serialNumber, String title, String gender, LocalDate releaseDate
+        , FilmType filmType, FilmStatus filmStatus, BigDecimal locationPrice) {
 }
