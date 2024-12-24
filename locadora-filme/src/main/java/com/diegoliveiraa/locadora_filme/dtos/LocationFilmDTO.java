f66948805583511d6a5547e949b5947b9490b5cf
Diego Oliveira <diegoliveiraa1@gmail.com>
2024-12-24 15:13:26 -0300

    feat: update DTO

--- locadora-filme/src/main/java/com/diegoliveiraa/locadora_filme/dtos/LocationFilmDTO.java
+++ locadora-filme/src/main/java/com/diegoliveiraa/locadora_filme/dtos/LocationFilmDTO.java
@@ -4,5 +4,6 @@ import com.diegoliveiraa.locadora_filme.entitys.Location;
 import com.diegoliveiraa.locadora_filme.infra.DevolutionStatus;
 import com.diegoliveiraa.locadora_filme.infra.FilmStatus;
 
-public record LocationFilmDTO(String filmId, Location locationId, DevolutionStatus devolutionStatus, FilmStatus filmStatus, Long quantityLocated) {
+public record LocationFilmDTO(String filmId, Location locationId, DevolutionStatus devolutionStatus,
+                              FilmStatus filmStatus, Long quantityLocated) {
 }
