c93ce9d227aee7a1ff517ec8a76dd863a4915700
Diego Oliveira <diegoliveiraa1@gmail.com>
2024-12-24 13:56:15 -0300

    feat: add new search

--- locadora-filme/src/main/java/com/diegoliveiraa/locadora_filme/repositories/FilmRepository.java
+++ locadora-filme/src/main/java/com/diegoliveiraa/locadora_filme/repositories/FilmRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface FilmRepository extends JpaRepository<Film, String> {
-    Optional<Film> findByTitle(String title);
+    Optional<Film> findByTitleOrGender(String title, String gender);
 
     Optional<Film> findById(String id);
 }
